682b69f097e41a0da5f0cf5f6d2fb18ece247faf
mikael-r <mikarg9@gmail.com>
2022-04-21 12:28:05 -0300

    fix: 🐛 domain replace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import StorePage from '@/components/templates/Store'
 import LandingPage from '@/components/templates/Landing'
 
 const Home = () => {
-  const splinted = window.location.host?.split('.')
+  const splinted = window.location.host
+    .replace('.com.br', '') // start replacing from the longest domain to lowest
+    .replace('.com', '')
+    .split('.')
 
   if (splinted.length > 1) {
     return <StorePage name={splinted[0]} />
